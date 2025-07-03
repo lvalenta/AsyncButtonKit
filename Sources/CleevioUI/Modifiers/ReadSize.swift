9bb6d017997accecf0f027f7291d383e20279745
Matej Simota <matej.simota@kswr.cz>
2025-07-03 14:12:00 +0200

    introducing size preference key with different reduce function implementation

--- Sources/CleevioUI/Modifiers/ReadSize.swift
+++ Sources/CleevioUI/Modifiers/ReadSize.swift
@@ -19,10 +19,31 @@ extension View {
         )
         .onPreferenceChange(SizePreferenceKey.self, perform: onChange)
     }
+    
+    @inlinable
+    public func readMaxSize(onChange: @Sendable @escaping (CGSize) -> Void) -> some View {
+        background(
+            GeometryReader { metrics in
+                Color.clear
+                    .preference(key: MaxSizePreferenceKey.self, value: metrics.size)
+            }
+        )
+        .onPreferenceChange(MaxSizePreferenceKey.self, perform: onChange)
+    }
+
+    
 }
 
 public struct SizePreferenceKey: PreferenceKey {
     public static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
+
+    public static let defaultValue: CGSize = .zero
+}
+
+public struct MaxSizePreferenceKey: PreferenceKey {
+    public static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+        value = max(value, nextValue())
+    }
 
     public static let defaultValue: CGSize = .zero
 }
